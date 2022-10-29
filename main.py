8372614eb0107d45fd748a35c1cea744b83b6920
mveyone <mveyone@outlook.fr>
2022-10-29 01:31:46 +0100

    new

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ devise='USD'
 calculate_to_mad = 1*10
 somme=int(input("say how much:?\n"))
 def converter(somme):
-    return print(f"it s {somme*calculate_to_mad} mad")
+    return print(f"it hs {somme*calculate_to_mad} mad")
 converter(somme)
